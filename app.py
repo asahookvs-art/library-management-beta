fd5bd85060a1acadece6defad38536de8f574052
asahookvs-art <asahookvs@gmail.com>
2026-01-01 21:39:38 +0530

    added port and improved login page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from APP import app,db
+from APP import app, db
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    
-    app.run(host='0.0.0.0')
+
+    app.run(host="0.0.0.0", port=5000)
